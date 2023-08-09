4cb22b809921639be8400b1bed2e502955aba3fb
André Abrão Martinson <141847383+andreabrao@users.noreply.github.com>
2023-08-09 14:33:42 -0300

    Create calculadoraBemSimples.js

---
+++ calculadoraBemSimples.js
@@ -0,0 +1,45 @@
+/*  PROJETO DE CALCULADORA BEM SIMPLES FEITO PÔR @github.com/andreabrao  COPIA E COLE NO VSCODE E RODE ATRAVÉS DO CONSOLE DO PROGRAMA   */
+
+/*MANUAL DE INSTRUÇÕES:
+Nas variáveis numUm e numDois digite 10 e coloque nas variáveis operadorUm e oMesmoOperadorUm a regra de ADICAO, SUBTRACAO, MULTIPLICACAO E DIVISAO e rode o código normalmente*/
+
+
+//••••  MEXER SOMENTE AQUI  ••••//
+//COLOCAR ALGUM VALOR NA VARIÁVEL numUm E numDois
+const numUm = 10
+const operadorUm = "divisao"
+const oMesmoOperadorUm = "divisao"
+const numDois = 10
+//••••  MEXER SOMENTE AQUI  ••••//
+
+
+//OPERADOR DE ADICAO
+if(operadorUm === oMesmoOperadorUm) {
+  console.log(numUm + numDois)
+}else {
+  console.log("Adicionar a palavra adicao sem assentos e caracteres especiais na variável operadorUm e oMesmoOperadorUm")
+}
+
+
+//OPERADOR DE SUBTRAÇÃO
+if(operadorUm === oMesmoOperadorUm) {
+  console.log(numUm - numDois)
+}else {
+  console.log("Adicionar a palavra subtracao sem assentos e caracteres especiais na variável operadorUm e oMesmoOperadorUm")
+}
+
+
+//OPERADOR DE MULTIPLICAÇÃO
+if(operadorUm === oMesmoOperadorUm) {
+  console.log(numUm * numDois)
+}else {
+  console.log("Adicionar a palavra multipliacacao sem assentos e caracteres especiais na variável operadorUm e oMesmoOperadorUm")
+}
+
+
+//OPERADOR DE DIVISÃO
+if(operadorUm === oMesmoOperadorUm) {
+  console.log(numUm / numDois)
+}else {
+  console.log("Adicionar a palavra divisao sem assentos e caracteres especiais na variável operadorUm e oMesmoOperadoUm")
+}
