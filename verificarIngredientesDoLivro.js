b5cfd3632edc2858c4f1eb594a7cc91625bc5db0
André Abrão Martinson <141847383+andreabrao@users.noreply.github.com>
2023-08-10 18:26:06 -0300

    Create verificarIngredientesDoLivro.js

---
+++ verificarIngredientesDoLivro.js
@@ -0,0 +1,68 @@
+    /*  PROJETO DE VERIFICADOR DE INGREDIENTES FEITO PÔR @github.com/andreabrao COPIA E COLE NO VSCODE E RODE ATRAVÉS DO CONSOLE DO PROGRAMA   */
+   
+  
+    //••• INGREDIENTES DA SUA GELADEIRA •••//
+    let ingredienteMolhado1 = ("margarina")
+    let ingredienteMolhado2 = ("leite")
+    let ingredienteSecos1 = ("açúcar")
+    let ingredienteSecos2 = ("nescau")
+    let ingredienteSecos3 = ("trigo")
+    let ingredienteSecos4 = ("fermento em pó")
+    
+    
+     //••• INGREDIENTES DO SEU LIVRO •••//
+    let ingredientesDoLivro1 = ("manteiga")
+    let ingredientesDoLivro2 = ("leite")
+    let ingredientesDoLivro_1 = ("açúcar") 
+    let ingredientesDoLivro_2= ("chocolate meio amargo")
+    let ingredientesDoLivro_3 = ("trigo")
+    let ingredientesDoLivro_4 = ("fermento pó")
+    
+    
+  
+    //••• VERIFICAR SE OS INGREDIENTES DA GELADEIRA SÃO IDÊNTICOS AOS DO LIVRO •••//
+    if(ingredienteMolhado1 === ingredientesDoLivro1) {
+      console.log("SIM, o ingrediente " + ingredienteMolhado1 + " é o mesmo ingrediente do livro")
+    }else {
+      console.log("NÃO, o ingrediente " + ingredienteMolhado1 + " não é o mesmo ingrediente do livro")
+    }
+    
+    ///
+    
+    if(ingredienteMolhado2 === ingredientesDoLivro2) {
+      console.log("SIM, o ingrediente " + ingredienteMolhado2 + " é o mesmo ingrediente do livro")
+    }else {
+      console.log("NÃO, o ingrediente " + ingredienteMolhado2 + " não é o mesmo ingrediente do livro")
+    }
+    
+    ///
+    
+    if(ingredienteSecos1 === ingredientesDoLivro_1) {
+      console.log("SIM, o ingrediente " + ingredienteSecos1 + " é o mesmo ingrediente do livro")
+    }else {
+      console.log("NÃO, o ingrediente " + ingredienteSecos1 + " não é o mesmo ingrediente do livro")
+    }
+    
+    ///
+
+    if(ingredienteSecos2 === ingredientesDoLivro_2) {
+      console.log("SIM, o ingrediente " + ingredienteSecos2 + " é o mesmo ingrediente do livro")
+    }else {
+      console.log("NÃO, o ingrediente " + ingredienteSecos2 + " não é o mesmo ingrediente do livro")
+    }
+    
+    ///
+    
+    if(ingredienteSecos3 === ingredientesDoLivro_3) {
+      console.log("SIM, o ingrediente " + ingredienteSecos3 + " é o mesmo ingrediente do livro")
+    }else {
+      console.log("NÃO, o ingrediente " + ingredienteSecos3 + " não é o mesmo ingrediente do livro")
+    }
+    
+    ///
+    
+    if(ingredienteSecos4 === ingredientesDoLivro_4) {
+      console.log("SIM, o ingrediente " + ingredienteSecos4 + " é o mesmo ingrediente do livro")
+    }else {
+      console.log("NÃO, o ingrediente " + ingredienteSecos4 + " não é o mesmo ingrediente do livro")
+    }
