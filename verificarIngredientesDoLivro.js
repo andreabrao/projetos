b7c30465543423ed9b852bb7a753d7472be332a7
André Abrão <141847383+andreabrao@users.noreply.github.com>
2023-08-11 11:47:47 -0300

    Atualizar o verificarIngredientesDoLivro.js
    Adicionado uma parte nova do código, a parte é modoDePreparo.
    
    Depois de verificar se os ingredientes escolhidos são os mesmos que do livro, iremos partir para o modo de preparo da receita👨‍💻☕😋

--- verificarIngredientesDoLivro.js
+++ verificarIngredientesDoLivro.js
@@ -66,3 +66,20 @@
     }else {
       console.log("NÃO, o ingrediente " + ingredienteSecos4 + " não é o mesmo ingrediente do livro")
     }
+
+
+    //PARTE NOVA
+    
+        //DIGITE 1 NA VARIÁVEL tudoCerto PARA CONFIRMAR QUE ESTÁ TUDO CERTO PARA SEGUIRMOS PARA O MODO DE PREPARO DA RECEITA
+    const tudoCerto = "1"
+    
+    
+    function modoDePreparo() {
+      if(tudoCerto == 1) {
+      console.log("Modo de preparo do bolo")
+      console.log(`Pegue uma colher de sopa raza de ${ingredientesDoLivro1}, e coloque na mesma xícara que vai usar, depois coloque no microondas por 1 minuto. Coloque uma colher de sopa de ${ingredientesDoLivro2}, junto com a manteiga derretida. Em seguida adicione duas colher de sopa de ${ingredientesDoLivro_1}, juntamente com duas colheres de sopa de ${ingredientesDoLivro_2} e mexa muito bem. Vá colocando duas colheres de sopa cheia de ${ingredientesDoLivro_3} aos poucos e mexa com um garfo, depois de mexer tudo muito bem coloque 1/3 de colher de café de ${ingredientesDoLivro_4} e mexa devagar, logo coloque no microondas por 1 minuto e pronto.`)
+    }
+    }
+    if(tudoCerto == 1) {
+      modoDePreparo()
+    }
