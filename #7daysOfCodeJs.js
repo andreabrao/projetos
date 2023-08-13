d56b011176a5b339cab312230d3d3a2a5a304810
André Abrão <141847383+andreabrao@users.noreply.github.com>
2023-08-13 20:07:35 -0300

    Create #7daysOfCodeJs.js

---
+++ #7daysOfCodeJs.js
@@ -0,0 +1,38 @@
+/*
+
+Reescrever o código abaixo para que ele imprima as informações de maneira correta e sem erros;
+
+Utilizar operadores de comparação do JavaScript no código para imprimir o resultado correto no console.
+
+*/
+
+let numberOne = 1
+let stringOne = '1'
+let numberTen = 10
+let stringTen = '10'
+let numberThirty = 30
+let stringThirty = '30'
+
+if(numberOne === 1) {
+  console.log(numberOne)
+}
+
+if(stringOne === 1) {
+  console.log(stringOne)
+}
+
+if(numberTen === 10) {
+  console.log(numberTen)
+}
+
+if(stringTen === 10) {
+  console.log(stringTen)
+}
+
+if(numberThirty === 30) {
+  console.log(numberThirty)
+}
+
+if(stringThirty === 30) {
+  console.log(stringThirty)
+}
